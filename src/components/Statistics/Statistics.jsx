2130e2e360a85b723ea5520af295b3fe7efc7dda
VladDyadenko <Stilmagroup@gmail.com>
2023-03-01 16:28:43 +0200

    react_hw_1_1

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types"
+import css from './Statistics.module.css'
+
+export const Statistics =({title,stats})=> {
+
+ return (
+ <section className={css.statistics} >
+    
+    {title && <h2 className={css.title}>{title}</h2>}
+
+    <ul className={css.statList} >
+        {stats.map(stats => (
+           <li className={css.item} key={stats.id} style={{backgroundColor: `${getRandomHexColor()}`}}>
+            <span className={css.label}>{stats.label}</span>
+            <span className={css.percentage}>{stats.percentage}</span>
+            </li>
+        ))}
+        
+    </ul>
+ </section>)
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+Statistics.propTypes = {
+    stats: PropTypes.arrayOf(
+        PropTypes.shape({
+          label: PropTypes.string.isRequired,
+          percentage: PropTypes.number.isRequired,
+        })
+    ),
+    title: PropTypes.string
+  }
